74f8b87f1fa198914e0b481016ff093248f0a3a3
unknown <yildizfe@outlook.com>
2021-11-09 16:07:40 +0300

    Deleted spaces

--- main.c
+++ main.c
@@ -15,41 +15,41 @@ GPIO_Init(GPIOC, &GPIO_InitStructure);
 }
 int main(void)
 {
-    gpio_ports_enable();
-		int state = 0; // 0 -> Red, 1 -> Yellow, 2 -> Green
-		int prev = 0;
-    while(1)
-    {
-			switch(state)
+gpio_ports_enable();
+int state = 0; // 0 -> Red, 1 -> Yellow, 2 -> Green
+int prev = 0;
+	while(1)
+	{
+		switch(state)
+		{
+			case(0):
 			{
-				case(0):
-				{
-					GPIO_SetBits(GPIOC, GPIO_Pin_13);
-					delay(5000);
-					GPIO_ResetBits(GPIOC, GPIO_Pin_13);
-					prev = 0;
-					state = 1;
-				}
-				case(1):
-				{
-					GPIO_SetBits(GPIOC, GPIO_Pin_12);
-					delay(2000);
-					GPIO_ResetBits(GPIOC, GPIO_Pin_12);
-					if(prev)
-						state = 0;
-					else
-						state = 2;
-				}
-				case(2):
-				{
-					GPIO_SetBits(GPIOC, GPIO_Pin_11);
-					delay(5000);
-					GPIO_ResetBits(GPIOC, GPIO_Pin_11);
-					prev = 2;
-					state = 1;
-				}
+				GPIO_SetBits(GPIOC, GPIO_Pin_13);
+				delay(5000);
+				GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+				prev = 0;
+				state = 1;
 			}
-		} 			
+			case(1):
+			{
+				GPIO_SetBits(GPIOC, GPIO_Pin_12);
+				delay(2000);
+				GPIO_ResetBits(GPIOC, GPIO_Pin_12);
+				if(prev)
+				state = 0;
+				else
+				state = 2;
+			}
+			case(2):
+			{
+				GPIO_SetBits(GPIOC, GPIO_Pin_11);
+				delay(5000);
+				GPIO_ResetBits(GPIOC, GPIO_Pin_11);
+				prev = 2;
+				state = 1;
+			}
+		}
+	} 			
 }
 /*Interrupt()
 {
